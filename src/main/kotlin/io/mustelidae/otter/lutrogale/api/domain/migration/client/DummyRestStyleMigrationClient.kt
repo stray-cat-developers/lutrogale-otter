4398f70c7cb9e4876a4448769f40a827c53c3d95
fennec <elijah17@gmail.com>
2024-12-01 20:37:53 +0900

    Fix error

--- src/main/kotlin/io/mustelidae/otter/lutrogale/api/domain/migration/client/DummyRestStyleMigrationClient.kt
+++ src/main/kotlin/io/mustelidae/otter/lutrogale/api/domain/migration/client/DummyRestStyleMigrationClient.kt
@@ -3,7 +3,8 @@ package io.mustelidae.otter.lutrogale.api.domain.migration.client
 import io.mustelidae.otter.lutrogale.api.domain.migration.openapi.SwaggerSpec
 
 class DummyRestStyleMigrationClient : RestStyleMigrationClient {
-    override fun getOpenAPISpec(url: String, type: SwaggerSpec.Type): String {
+    override fun getOpenAPISpec(url: String, type: SwaggerSpec.Type, headers: List<Pair<String, Any>>?): String {
         TODO("Not yet implemented")
     }
+
 }
