cecccde20d7e053e7578649705259191d5043552
fennec(서오석) <fennec.fox3@kakaostyle.com>
2024-12-03 14:05:13 +0900

    Fix lint

--- src/test/kotlin/io/mustelidae/otter/lutrogale/api/domain/migration/api/APISpecMigrationControllerFlow.kt
+++ src/test/kotlin/io/mustelidae/otter/lutrogale/api/domain/migration/api/APISpecMigrationControllerFlow.kt
@@ -6,7 +6,6 @@ import io.mustelidae.otter.lutrogale.utils.toJson
 import org.springframework.hateoas.server.mvc.linkTo
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.get
 import org.springframework.test.web.servlet.post
 
 class APISpecMigrationControllerFlow(
